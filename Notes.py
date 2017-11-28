b2c7ed905cdc7840bb81e27ceb4ec5e575fff192
n7ma <34071931+n7ma@users.noreply.github.com>
2017-11-28 10:47:51 -0800

    Added Notes 11/27/2017
    11/28/2017

--- Notes.py
+++ Notes.py
@@ -1,1 +1,18 @@
 #this is  working
+print(15 % 5)
+print(14 % 5)
+print(13 % 5)
+print(12 % 5)
+print(11 % 5)
+print(10 % 5)
+print(9 % 5)
+print(5 % 3)
+
+
+car_name = "Weibe Mobile"
+car_type = "Nissan GTR"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s" The GTR)
