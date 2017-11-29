3f85b237cef0215fb4bc7bc3e90c968fc8604fbf
n7ma <34071931+n7ma@users.noreply.github.com>
2017-11-29 10:49:07 -0800

    added function

--- Notes.py
+++ Notes.py
@@ -16,3 +16,50 @@ car_mpg = 9000.1
 
 # Inline printing
 print("I have a car called the %s" The GTR)
+
+# Function
+
+]
+def print_hw():
+    print('Hello World"')
+
+
+print_hw()
+print_hw()
+print_hw()
+
+def say_hi(name): # nameis a premeter
+print('Hello ')
+
+say_hi("Jimmy")
+
+
+def print_age(name, age):
+    print("%s is %d years old" % (name, age))
+
+
+# if statement
+
+
+
+def f(x):
+     return x**3 +4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage < 90 and percentage >=80 :
+        return "8"
+    elif percentage >= 70 :
+        return "c"
+
+
+
+    
